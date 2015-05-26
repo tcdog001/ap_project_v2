26c4f52dc1c9864e1b121cf2505c189b9c4b94c6
guoshuai <guoshuai@debian6.(none)>
2015-05-26 18:03:31 -0400

    modify for network_local.sh

--- qsdk/files/sbin/network_local.sh
+++ qsdk/files/sbin/network_local.sh
@@ -3,6 +3,7 @@ main() {
 	cp /etc/config/network.dev /etc/config/network
 	/etc/init.d/network reload
 	/etc/jsock/jcmd.sh syn "stop_3g"
+	/etc/jsock/jcmd.sh syn "route del default ppp0" 2>/dev/null
 	/etc/jsock/jcmd.sh syn "route add default gw 1.0.0.1" 2>/dev/null
 }
 
